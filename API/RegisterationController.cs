46206006c12cd72c2f88e6dcdf0cd3f70b2c963d
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2021-02-14 23:43:10 +0530

    first

---
+++ API/RegisterationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registration_Form.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterationController : ControllerBase
+    {
+    }
+}
---
+++ DB/DBRegister.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registration_Form.DB
+{
+    public class DBRegister
+    {
+    }
+}
